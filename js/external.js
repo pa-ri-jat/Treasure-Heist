dd5ddc1854d15506a285ea9040d40aec7d35d166
pa-ri-jat <parijatbiswas135@gmail.com>
2022-02-17 22:19:10 +0600

    changed

---
+++ js/external.js
@@ -0,0 +1,11 @@
+document.getElementById('calculation').addEventListener('click',function(){
+    const foodField = document.getElementById('food');
+    const userFood = incomeField.value;
+    console.log(userFood);
+    const rentField = document.getElementById('rent');
+    const userRent = rentField.value;
+    console.log(userRent);
+    const clothesField = document.getElementById('clothes');
+    const userClothes = clothesField.value;
+    console.log(userClothes);
+})
